2bc16c029b2128ab46e625bf5737f072865a9753
Mariane Algayer <marianealgayer@gmail.com>
2021-11-05 18:59:56 -0300

    Logical operators/or exercise solution

--- 01-introduction/04-introduction-to-javascript-programming-logic/day-1-first-steps/04-logical-operators-and.js
+++ 01-introduction/04-introduction-to-javascript-programming-logic/day-1-first-steps/04-logical-operators-and.js
@@ -9,7 +9,7 @@ if(currentHour >= 22) {
     message = 'Vamos fazer um bolo pro café da tarde?';
 } else if(currentHour >= 11 && currentHour < 14) {
     message = 'Hora do almoço!!!';
-} else if(currentHour >= 4 && currentHour < 11) {
+} else if(currentHour > 4 && currentHour < 11) {
     message = 'Hmmm, cheiro de café recém passado';
 }
 
