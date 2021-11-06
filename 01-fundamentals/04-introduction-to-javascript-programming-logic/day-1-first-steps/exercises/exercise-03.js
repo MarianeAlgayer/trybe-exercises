a82b436cac0365c1bc537091720eaaead712a92b
Mariane Algayer <marianealgayer@gmail.com>
2021-11-06 16:08:16 -0300

    Exercise-03 solution

---
+++ 01-fundamentals/04-introduction-to-javascript-programming-logic/day-1-first-steps/exercises/exercise-03.js
@@ -0,0 +1,11 @@
+const a = 40;
+const b = 50;
+const c = 60;
+
+if(a > b && a > c) {
+    console.log(`O número ${a} é maior do que ${b} e ${c}.`)
+} else if(b > a && b > c) {
+    console.log(`O número ${b} é maior do que ${a} e ${c}.`)
+} else {
+    console.log(`O número ${c} é maior do que ${a} e ${b}.`)
+}
