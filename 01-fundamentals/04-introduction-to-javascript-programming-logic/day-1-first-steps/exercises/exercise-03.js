dac9c0bce137e8d241bca96848242ee0c1763b73
Mariane Algayer <marianealgayer@gmail.com>
2021-11-06 16:17:26 -0300

    Update exercises 01 and 03

--- 01-fundamentals/04-introduction-to-javascript-programming-logic/day-1-first-steps/exercises/exercise-03.js
+++ 01-fundamentals/04-introduction-to-javascript-programming-logic/day-1-first-steps/exercises/exercise-03.js
@@ -3,9 +3,9 @@ const b = 50;
 const c = 60;
 
 if(a > b && a > c) {
-    console.log(`O número ${a} é maior do que ${b} e ${c}.`)
+    console.log(`O número ${a} é maior do que ${b} e ${c}.`);
 } else if(b > a && b > c) {
-    console.log(`O número ${b} é maior do que ${a} e ${c}.`)
+    console.log(`O número ${b} é maior do que ${a} e ${c}.`);
 } else {
-    console.log(`O número ${c} é maior do que ${a} e ${b}.`)
+    console.log(`O número ${c} é maior do que ${a} e ${b}.`);
 }
