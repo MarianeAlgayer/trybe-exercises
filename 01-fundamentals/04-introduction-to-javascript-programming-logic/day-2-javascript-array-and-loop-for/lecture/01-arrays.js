9d24297b3c7a09af6b00e9dd2d29ecdd8868f0cf
Mariane Algayer <marianealgayer@gmail.com>
2021-11-07 18:16:51 -0300

    Arrays exercise solution

---
+++ 01-fundamentals/04-introduction-to-javascript-programming-logic/day-2-javascript-array-and-loop-for/lecture/01-arrays.js
@@ -0,0 +1,20 @@
+// Exercise 1: Obtenha o valor "Serviços" do array menu.
+
+/* let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices); */
+
+// Exercise 2: Procure o índice do valor "Portfólio" do array menu.
+
+/* let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio); */
+
+// Exercise 3: Adicione o valor "Contato" no final do array menu.
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Contato');
+
+console.log(menu);
