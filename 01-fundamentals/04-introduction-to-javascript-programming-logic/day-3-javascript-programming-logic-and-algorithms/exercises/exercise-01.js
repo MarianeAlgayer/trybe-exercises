2c8d9fbed89e5235c95cb77f631f02577fb0feb0
Mariane Algayer <marianealgayer@gmail.com>
2021-11-09 11:52:25 -0300

    Exercise-01 solution

---
+++ 01-fundamentals/04-introduction-to-javascript-programming-logic/day-3-javascript-programming-logic-and-algorithms/exercises/exercise-01.js
@@ -0,0 +1,17 @@
+let n = 5;
+
+if(n > 1) {
+    for(index = 1; index <= n; index += 1) {
+        console.log('*'.repeat(n));
+    }
+}
+
+/* let symbol = '*';
+let inputLine = '';
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  inputLine = inputLine + symbol;
+}
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  console.log(inputLine);
+} */
