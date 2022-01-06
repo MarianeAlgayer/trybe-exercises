55de5040e9094727b67e7b89fe92a0716451b97d
Mariane Algayer <marianealgayer@gmail.com>
2022-01-06 16:45:05 -0300

    Exercise solutions

---
+++ 01-fundamentals/08-javascript-es6-higher-order-functions/day-3-javascript-es6-higher-order-functions-map-filter/lecture/filter.js
@@ -0,0 +1,10 @@
+const arrayMyStudents = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
+
+const removeStudentByName = (name, listStudents) =>
+  listStudents.filter((student) => student !== name);
+  // Filtra todos os estudantes que não têm o nome 'Ricardo' e retorna um array com eles. Na prática, remove o Ricardo do array.
+
+const newListStudents = removeStudentByName('Ricardo', arrayMyStudents);
+console.log(newListStudents); // [ 'Maria', 'Manuela', 'Jorge', 'Wilson' ]
+
+// Observe que o filter foi usado dentro de uma função que recebe dois parâmetros, o array de valores e uma string, o que será removido. A condição de dentro do filter é para retornar sempre que o elemento for diferente do name passado.
